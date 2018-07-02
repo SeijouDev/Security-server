e72e083a5d54019153932e3bd278e7724e0dc270
seijoudev <alejo.1395@hotmail.com>
2018-07-02 01:57:33 -0500

    pg connection fixed

--- models/user.js
+++ models/user.js
@@ -1,11 +1,25 @@
 const pg = require('pg');
 const { Client } = require('pg');
+
+// const client = new Client({
+//   user: 'postgres',
+//   host: 'localhost',
+//   database: 'securitydb',
+//   password: 'root',
+//   port: 5432,
+// });
+
+// const client = new Client({
+//     user: 'jhkhtqqykdvqau',
+//     host: 'ec2-54-197-233-123.compute-1.amazonaws.com',
+//     database: 'd1kr6oe2l4lp9f',
+//     password: '903cf10387afabb757c8bf42baae3ad3ac6de647dfd721f642e622a18e859fdf',
+//     port: 5432,
+//   });
+
 const client = new Client({
-  user: 'postgres',
-  host: 'localhost',
-  database: 'securitydb',
-  password: 'root',
-  port: 5432,
+    connectionString: "postgres://jhkhtqqykdvqau:903cf10387afabb757c8bf42baae3ad3ac6de647dfd721f642e622a18e859fdf@ec2-54-197-233-123.compute-1.amazonaws.com:5432/d1kr6oe2l4lp9f",
+    ssl: true
 });
 
 client.connect().then( ()=> {
