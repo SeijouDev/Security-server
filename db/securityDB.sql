514e792891c05fc44e26153054f1e7f83f0b0e7c
seijoudev <alejo.1395@hotmail.com>
2018-07-02 01:13:53 -0500

    mysql to pg

--- db/securityDB.sql
+++ db/securityDB.sql
@@ -10,3 +10,28 @@ CREATE TABLE `securitydb`.`users` (
 
 INSERT INTO `securitydb`.`users` (`name`, `email`, `password`)
  VALUES ('Marlon Castillo', 'alejo.1395@hotmail.com', '123456');
+
+
+ /********************/
+
+-- Database: securitydb
+
+-- DROP DATABASE securitydb;
+
+CREATE DATABASE securitydb
+    WITH 
+    OWNER = postgres
+    ENCODING = 'UTF8'
+    LC_COLLATE = 'Spanish_Spain.1252'
+    LC_CTYPE = 'Spanish_Spain.1252'
+    TABLESPACE = pg_default
+    CONNECTION LIMIT = -1;
+
+CREATE TABLE users (
+  id SERIAL PRIMARY KEY,
+  name VARCHAR(45) NOT NULL,
+  email VARCHAR(45) NOT NULL UNIQUE,
+  password VARCHAR(45) NOT NULL);
+
+INSERT INTO users (name,email, password) 
+VALUES ('Marlon Castillo','alejo.1395@hotmail.com', '123456');
--- models/user.js
+++ models/user.js
@@ -1,51 +1,58 @@
-const mysql = require('mysql');
+const pg = require('pg');
+const { Client } = require('pg');
+const client = new Client({
+  user: 'postgres',
+  host: 'localhost',
+  database: 'securitydb',
+  password: 'root',
+  port: 5432,
+});
 
-connection = mysql.createConnection({
-    host: 'localhost',
-    user: 'root',
-    password: 'root',
-    database: 'securitydb'
+client.connect().then( ()=> {
+    console.log("Connected to pg!");
+}).catch( err => {
+    console.log("Error on pg connection:");
+    console.log(err);
 });
 
 let userModel = {
 
     findAll: (callback) => {
-        if(connection) {
-            connection.query('SELECT id, name, email, password FROM users;' , (err, rows) => {
+        if(client) {
+            client.query('SELECT id, name, email, password FROM users;' , (err, result) => {
                 if(err){
                     throw(err);
                 }
                 else {
-                    callback(null, rows);
+                    callback(null, result.rows);
                 }
             });
         }
     },
 
     findOne: (id, callback) => {
-        if(connection) {
-            connection.query('SELECT id, name, email, password FROM users WHERE id = ?;' , id , (err, rows) => {
+        if(client) {
+            client.query('SELECT id, name, email, password FROM users WHERE id = $1;' , [id] , (err, result) => {
                 if(err){
                     throw(err);
                 }
                 else {
-                    callback(null, rows);
+                    callback(null, result.rows);
                 }
             });
         }
     },
 
     insert: (user, callback) => {
-        if(connection) {
-            connection.query('INSERT INTO users SET ?', user, (err, result) => {
+        if(client) {
+            client.query("INSERT INTO users (name,email, password) VALUES ($1,$2,$3) RETURNING id;", [user.name, user.email, user.password], (err, result) => {
                 if(err) {
                     throw err;
                 }
                 else {
-                    callback(null, { result: result.insertId});
+                    callback(null, { result: result.rows[0].id});
                 }
             });
-
         }
     }
 };
